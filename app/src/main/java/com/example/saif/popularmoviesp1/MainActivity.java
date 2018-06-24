362e756501b544de7bca58e6058abb4f630e8723
Shaik Saif Ali <saifshine7@gmail.com>
2018-06-24 20:10:07 +0530

    Directly testing with API

--- app/src/main/java/com/example/saif/popularmoviesp1/MainActivity.java
+++ app/src/main/java/com/example/saif/popularmoviesp1/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.saif.popularmoviesp1;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.res.Configuration;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import com.example.saif.popularmoviesp1.adapter.MovieAdapter;
 import com.example.saif.popularmoviesp1.model.Movie;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,10 +47,41 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void initViews(){
-        pd =  new ProgressDialog(this);
+    public Activity getActivity(){
+        Context context = this;
+        while (context instanceof ContextWrapper){
+            if(context instanceof Activity){
+                return (Activity) context;
+            }
+            context = ((ContextWrapper) context).getBaseContext();
+        }
+        return null;
+    }
+
+    private void initViews() {
+        pd = new ProgressDialog(this);
         pd.setMessage("Bringing Movies to you..");
         pd.setCancelable(false);
         pd.show();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        movieList = new ArrayList<>();
+        adapter = new MovieAdapter(this.movieList);
+
+        if (getActivity().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        } else {
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
+        }
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+    }
+
+    private void loadJSON(){
+        
     }
 }
