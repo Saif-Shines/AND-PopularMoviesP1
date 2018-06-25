e069e94f08831d8b44f509b7d04aa85c5aaa5bc5
Shaik Saif Ali <saifshine7@gmail.com>
2018-06-25 13:50:10 +0530

    Movie class is filling up the variables

--- app/src/main/java/com/example/saif/popularmoviesp1/model/Movie.java
+++ app/src/main/java/com/example/saif/popularmoviesp1/model/Movie.java
@@ -2,11 +2,44 @@ package com.example.saif.popularmoviesp1.model;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Movie {
     @SerializedName("poster_path")
     private String posterPath;
     @SerializedName("adult")
     private boolean adult;
     @SerializedName("overview")
-    private
+    private String overview;
+    @SerializedName("release_date")
+    private String releaseDate;
+    @SerializedName("genre_ids")
+    private List<Integer> genreIds = new ArrayList<Integer>();
+    @SerializedName("id")
+    private Integer id;
+    @SerializedName("original_title")
+    private String originalTitle;
+    @SerializedName("original_language")
+    private String originalLanguage;
+    @SerializedName("title")
+    private String title;
+    @SerializedName("backdrop_path")
+    private String backdropPath;
+    @SerializedName("popularity")
+    private Double populartity;
+    @SerializedName("vote_count")
+    private Integer voteCount;
+    @SerializedName("video")
+    private Boolean video;
+    @SerializedName("vote_average")
+    private Double voteAverage;
+
+    public Movie(String posterPath,boolean adult,String overview,String releaseDate,List<Integer> genreIds, Integer id,
+                 String originalTitle,String originalLanguage,String title, String backdropPath, Double populartity,
+                 Integer voteCount,Boolean video, Double voteAverage){
+        this.posterPath = posterPath;
+        this.
+    }
+
 }
