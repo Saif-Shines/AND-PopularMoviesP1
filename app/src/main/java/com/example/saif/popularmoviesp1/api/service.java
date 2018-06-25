42c93703b5bd977b413b0aaab96aff813648439f
Shaik Saif Ali <saifshine7@gmail.com>
2018-06-25 13:40:28 +0530

    Building movie.java to take from movie api

--- app/src/main/java/com/example/saif/popularmoviesp1/api/service.java
+++ app/src/main/java/com/example/saif/popularmoviesp1/api/service.java
@@ -1,4 +1,18 @@
 package com.example.saif.popularmoviesp1.api;
 
-public class service {
+import com.example.saif.popularmoviesp1.adapter.MovieAdapter;
+import com.example.saif.popularmoviesp1.model.MovieResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface service {
+
+    @GET("movie/popular")
+    Call<MovieAdapter> getPopularMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/top_rated")
+    Call<MovieResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
 }
--- app/src/main/java/com/example/saif/popularmoviesp1/model/Movie.java
+++ app/src/main/java/com/example/saif/popularmoviesp1/model/Movie.java
@@ -1,4 +1,12 @@
 package com.example.saif.popularmoviesp1.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Movie {
+    @SerializedName("poster_path")
+    private String posterPath;
+    @SerializedName("adult")
+    private boolean adult;
+    @SerializedName("overview")
+    private
 }
