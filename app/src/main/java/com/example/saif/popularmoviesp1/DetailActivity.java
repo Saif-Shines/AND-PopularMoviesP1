b56d6bbf141d2d760564a53052525645431ae84b
Shaik Saif Ali <saifshine7@gmail.com>
2018-06-26 23:40:45 +0530

    gotcha Detatil Acitvity

--- app/src/main/java/com/example/saif/popularmoviesp1/DetailActivity.java
+++ app/src/main/java/com/example/saif/popularmoviesp1/DetailActivity.java
@@ -1,4 +1,28 @@
 package com.example.saif.popularmoviesp1;
 
-public class DetailActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+public class DetailActivity extends AppCompatActivity{
+    TextView nameOfMovie, plotSynopsis, userRating, releaseDate;
+    ImageView imageView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        initCollapsingToolbar();
+
+        imageView = (ImageView) findViewById(R.id.thumbail_image_header);
+        nameOfMovie = (TextView) findViewById(R.id.title);
+        plotSynopsis = (TextView)
+    }
 }
